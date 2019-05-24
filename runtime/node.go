4fb935759f85c678f80400e0340242143c5e2aaa
kenkao <kenkao_01@126.com>
2019-05-24 15:40:39 +0800

    fix

--- runtime/node.go
+++ runtime/node.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+
+	//	"github.com/golang/protobuf/proto"
 	"github.com/itfantasy/gonode"
 	"github.com/itfantasy/gonode/behaviors/gen_server"
 )
@@ -26,7 +29,8 @@ func (this *GridNode) OnConn(id string) {
 }
 func (this *GridNode) OnMsg(id string, msg []byte) {
 	gonode.Send(id, msg)
+
 }
 func (this *GridNode) OnClose(id string) {
-
+	fmt.Println("conn has been closed! " + id)
 }
